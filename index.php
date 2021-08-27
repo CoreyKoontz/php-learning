ec71de936c3547faf1f2380f641ad3cef0584c8a
Corey Koontz <corey.michael.koontz@gmail.com>
2021-08-27 16:50:38 -0500

    LAracast php variables lesson

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+// Use command: 'php -S localhost:8888' to start a local server on
+// port 8888. Allowing you to see the results in your browser.
+
+// ---------------------------------------------------------- Variables
+// Variables in php begin with '$':
+$name = 'Corey';
+$greeting = 'Hello';
+echo "$greeting, $name"; // Must use double quotes when using multiple variables in a string.
+echo "Hello, {$name}"; // Sometimes you will see the variable wrapped in braces, but it is not necessary
+echo $greeting . ' ' . $name; // concatenation is with "." instead of "+" in php.
+
+
+
